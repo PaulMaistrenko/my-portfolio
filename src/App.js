e6cf87554ff534401155d23c089091d1adfba6d5
Pavel Maistrenko <maystrenko.p@gmail.com>
2023-01-20 15:30:17 +0100

    first chenges

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +14,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>Хостинг по красоте</p>
       </header>
     </div>
   );
